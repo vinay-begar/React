fe1d203ce8f260a8504dcacbcf3787a0f8a86b4c
Vinay Begar <vinaybbegar@gmail.com>
2025-11-17 20:03:50 +0530

    css modules, props, render lists in react updated!

--- 02 - React CSS/module-css/src/App.jsx
+++ 02 - React CSS/module-css/src/App.jsx
@@ -1,7 +1,11 @@
+import style from "./App.module.css";
+
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div className={style.container}>
+      <h1 className={style.heading}>Vinay Begar</h1>
+    </div>
+  );
+};
 
-export default App
+export default App;
